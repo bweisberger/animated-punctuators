8cd4364ff22e14b843b3a47bea50b29252a17309
Bill Weisberger <bill.weisberger@gmail.com>
2021-04-02 20:30:24 -0600

    - header and footer components - third png - added vue-router

--- src/main.js
+++ src/main.js
@@ -1,10 +1,39 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import VueRouter from 'vue-router';
 import App from './App.vue';
 
 Vue.config.productionTip = false
 
 Vue.use(Vuex);
+Vue.use(VueRouter);
+
+// 0. If using a module system (e.g. via vue-cli), import Vue and VueRouter
+// and then call `Vue.use(VueRouter)`.
+
+// 1. Define route components.
+// These can be imported from other files
+const About = { template: '<div>About</div>' }
+const Home = { template: '<div>Home</div>' }
+
+// 2. Define some routes
+// Each route should map to a component. The "component" can
+// either be an actual component constructor created via
+// `Vue.extend()`, or just a component options object.
+// We'll talk about nested routes later.
+const routes = [
+  { path: '/about', component: About },
+  { path: '/home', component: Home }
+]
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
+
+// Now the app has started!
 
 const store = new Vuex.Store({
   state: {
@@ -17,5 +46,6 @@ const store = new Vuex.Store({
 
 new Vue({
   store,
+  router,
   render: h => h(App),
 }).$mount('#app')
